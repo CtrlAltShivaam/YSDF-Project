7c416b26f06178698302aebda0d4397c7253a84c
Shivam Sharma <64611917+CtrlAltShivaam@users.noreply.github.com>
2023-03-29 23:48:08 +0530

    File perfectly ordered

--- Header/header-script.js
+++
@@ -1,8 +0,0 @@
-let typingEffect = new Typed('.multitext', {
-    strings : ["Govt. of India", "NITI Aayog", "Skill India", 
-                "Digital India", "Govt. of West Bengal", "ISO 9001 Certified", "MSME & More"],
-    loop : true,
-    typeSpeed : 40,
-    backSpeed : 30,
-    backDelay : 1000
-})
